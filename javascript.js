026efb91c290eb2815f7c4bab27d8b43bddc6a96
yvanpolimon19-stack <yvanpolimon19@gmail.com>
2026-01-15 10:35:49 +0400

    added the button just have to add a function to it

--- javascript.js
+++ javascript.js
@@ -1,7 +1,28 @@
 const main = document.querySelector(".container");
 
+
+
+function handleMouseEnter(){
+    const element = event.target;
+    element.classList.add("hovered");
+}
+function handleMouseLeave(){
+    const element = event.target;
+    element.classList.remove("hovered");
+}
+
+
 for(let i = 1; i <= 256; i++){
     const square = document.createElement("div");
+    
     square.classList.add("square");
+
     main.appendChild(square);
-}
+};
+
+const squares = document.querySelectorAll(".square");
+squares.forEach((element)=>{
+    element.addEventListener("mouseover",handleMouseEnter);
+    element.addEventListener("mouseout",handleMouseLeave);
+});
+
